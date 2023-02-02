2c68351a630ef7c3eaa9a4899670cfe7f0f3b03f
gangamgr <gangamagar643@gmail.com>
2023-02-02 12:13:29 +0545

    complete

--- calc.js
+++ calc.js
@@ -9,15 +9,14 @@ function Value(a){
         show.value=first;
     }else{
         last+=a;
-        show.value+=last;
+        show.value+=a;
     }
     console.log(first,last,opertors)
 }
 
 function Opt(a){
-    opertors=a;
-    // console.log(opertors)
-    show.value+=opertors;
+    opertors+=a;
+    show.value+=a;
 }
 
 function Result(a){
@@ -44,4 +43,25 @@ opertors="";
 show.value="0";
 }
 
-
+function Partial(){
+    if(opertors.length===0){
+        let lgt=first.length;
+        let data=first.slice(0,lgt-1);
+        console.log(first,data);
+        first=data;
+        show.value=data;
+    }else if(opertors.length>=1 && last.length===0){
+        let lgt=opertors.length;
+        console.log(lgt)
+        let data=opertors.slice(0,lgt-1);
+        console.log(opertors,data);
+        opertors=data;
+        show.value=first+opertors
+    } else{
+        let lgt=last.length;
+        let data=last.slice(0,lgt-1);
+        console.log(last,data,opertors);
+        last=data;
+        show.value=first+opertors+last;
+    }
+}
